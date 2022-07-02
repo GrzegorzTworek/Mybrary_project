19dd5777ae34614ae6aabd0d5d02b1985a7f0600
GrzegorzTworek <gt.grzegorz.tworek@gmail.com>
2022-07-02 04:00:03 +0200

    Create/Index/New

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const Book = require("../models/book");
 
-router.get("/", (req, res) => {
-  // res.send("Hello worldd");
-  res.render("index");
-  // res.send("Hello worldd");
+router.get("/", async (req, res) => {
+  let books;
+  try {
+    books = await Book.find().sort({ createdAt: "desc" }).limit(10).exec();
+  } catch {
+    books = [];
+  }
+  res.render("index", { books: books });
 });
 
 module.exports = router;
